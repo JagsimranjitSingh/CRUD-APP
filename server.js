0ff70a3ea1b9953c2c25149e1f384ac807439d21
JagsimranjitSingh <jaskainth38@gmail.com>
2024-11-21 16:47:18 +0530

    Add HTML file with Bootstrap Library

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ connect.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    age : Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', async (request, response) => {
+    response.sendFile(__dirname + '/user.html');
+});
+
+app.post('/users', async (request, response) => {
+    const user = new User({
+        name : request.body.name,
+        email : request.body.email,
+        age : request.body.age
+    });
+    const newItem = await user.save();
+    response.status(201).json({scuccess:true});
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })

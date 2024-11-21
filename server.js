1ecf939d3d140027aca33821847d666c25e5905d
JagsimranjitSingh <jaskainth38@gmail.com>
2024-11-21 17:02:50 +0530

    Change files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
 
-mongoose.connect('mongodb+srv://jaskainth38:hEKqzCn7soXmE79u@cluster0.c8o2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://johnsmith174:chb9TegHolwI69JU@cluster0.8dbkdwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const connect = mongoose.connection;
 
@@ -41,6 +41,35 @@ app.post('/users', async (request, response) => {
     response.status(201).json({scuccess:true});
 });
 
+app.get('/users', async (request, response) => {
+    const users = await User.find();
+    response.status(200).json(users);
+});
+
+app.get('/users/:id', async (request, response) => {
+    const user = await User.findById(request.params.id);
+    response.status(200).json(user);
+});
+
+app.put('/users/:id', async (request, response) => {
+    const userId = request.params.id;
+    // Fetch the user from the database
+    const user = await User.findById(userId);
+    user.name = request.body.name;
+    user.email = request.body.email;
+    user.age = request.body.age;
+    const updatedItem = await user.save();
+    response.status(200).json(updatedItem);
+});
+
+app.delete('/users/:id', async (request, response) => {
+    const userId = request.params.id;
+    // Fetch the user from the database
+    const user = await User.findById(userId);
+    await user.deleteOne();
+    response.status(200).json({ message : 'Deleted item' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
